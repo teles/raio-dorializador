aeb77716dc3dc833f5f6a3d17accfbafda277ae0
Rafael Kato <rafael@kato.io>
2017-01-24 23:48:47 -0200

    Implementando toggle do plugin no click do botão e adicionando icones

--- raio-dorializador.js
+++ raio-dorializador.js
@@ -1,21 +1,38 @@
-function changeAllImagesToGrey () {
-	var images = Array.from(document.getElementsByTagName("img"));
-	images.forEach(function (i) {
-		i.removeAttribute('src');
-		i.style.backgroundColor = '#a5a0a0';
-	});
-
+function changeAllImagesToGrey(currentState) {
+  var images = Array.from(document.getElementsByTagName('img'));
+  images.forEach(function(i) {
+    if (currentState === 'on') {
+      i.dataset.src = i.getAttribute('src');
+      i.removeAttribute('src');
+      i.style.backgroundColor = '#a5a0a0';
+    } else {
+      if (i.dataset.src) {
+        i.setAttribute('src', i.dataset.src);
+      }
+      i.style.backgroundColor = 'transparent'
+    }
+  });
 }
 
-function makeEverythingGrayscale () {
-	document.getElementsByTagName("html")[0].style.filter = "grayscale(100%)";
+function makeEverythingGrayscale(currentState) {
+  if (currentState === 'on') {
+    document.getElementsByTagName('html')[0].style.filter = 'grayscale(100%)';
+  } else {
+    document.getElementsByTagName('html')[0].style.filter = 'none';
+  }
 }
 
-changeAllImagesToGrey();
-makeEverythingGrayscale();
+function run(chrome) {
+  chrome.storage.local.get(function(items) {
+    changeAllImagesToGrey(items.currentState);
+    makeEverythingGrayscale(items.currentState);
+  });
+}
 
-(function (XHR) {
-    XHR.prototype.open = function () {
-        changeAllImagesToGrey();
-    };
-})(XMLHttpRequest);
+(function(XHR, chrome) {
+  XHR.prototype.open = function() {
+    run(chrome);
+  };
+})(XMLHttpRequest, chrome);
+
+run(chrome);
